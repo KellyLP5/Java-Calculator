cb0613fae9a375fcca144b760a45fce7dce2b1d4
Kelly Payne <166059714+KellyLP5@users.noreply.github.com>
2026-01-05 11:59:11 -0500

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -15,6 +15,11 @@ public class Calculator {
 		System.out.println("- for Subtraction");
 		System.out.println("* for Multiplication");
 		System.out.println("/ for Division");
+		System.out.println("s Square Root");
+		System.out.println("^ Power");
+		System.out.println("n Sin");
+		System.out.println("c Cosine");
+		System.out.println("t Tangent");
 		
 		char operator = scanner.next().charAt(0);
 		double result;
@@ -43,7 +48,36 @@ public class Calculator {
 				System.out.println("Error");
 			}
 			break;
-		default:
+			
+		case '^': result = Math.pow(num1, num2); 
+			System.out.println("Result: " + result);
+			break; 
+			
+		case 's':
+			System.out.println("Enter number: ");
+			double x = scanner.nextDouble();
+			System.out.println("Result: " + Math.sqrt(x));
+			break;
+		
+		 case 'n':
+             System.out.print("Enter angle in degrees: ");
+             double angle = scanner.nextDouble();
+             System.out.println("Result: " + Math.sin(Math.toRadians(angle)));
+             break;
+        
+		 case 'c':
+             System.out.print("Enter angle in degrees: ");
+             angle = scanner.nextDouble();
+             System.out.println("Result: " + Math.cos(Math.toRadians(angle)));
+             break;
+             
+		 case 't':
+             System.out.print("Enter angle in degrees: ");
+             angle = scanner.nextDouble();
+             System.out.println("Result: " + Math.tan(Math.toRadians(angle)));
+             break;
+             
+         default:
 			System.out.println("Invalid operation");
 				
 		}
